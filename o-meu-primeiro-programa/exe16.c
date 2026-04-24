afe03520682e215d5065ff9f45ec0e3ddc274a97
Wesley Souza <wesley.git.ps@gmail.com>
2026-04-24 17:15:45 -0300

    Renomeando o capitúlo de introdução

---
+++ o-meu-primeiro-programa/exe16.c
@@ -0,0 +1,17 @@
+/**
+ * Escreva um programa que coloque na tela uma árvore com o seguinte formato:
+      *
+     ***
+    *****
+     /|\
+ */
+
+#include <stdio.h>
+
+void main(void) {
+   printf("  *\n");
+   printf(" ***\n");
+   printf("*****\n");
+   printf(" /|\\\n");
+}
+
