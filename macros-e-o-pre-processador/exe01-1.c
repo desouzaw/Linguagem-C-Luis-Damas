bf13bd09a796ecb859b0427aaa3ada6afe6776bc
Wesley Souza <wesley.git.ps@gmail.com>
2026-05-25 23:23:44 -0300

    exe01-1 - capítulo: macros e o pré-processador - Resolvido

---
+++ macros-e-o-pre-processador/exe01-1.c
@@ -0,0 +1,29 @@
+/**
+ * Suponha as seguintes constantes simbólicas declaradas
+ * 
+ * #define TAXA_ICMS1 5
+ * #define TAXA_ICMS2 17
+ * 
+ * Defina a macro Val_ICMS(Salario) que deverá devolver o valor do ICMS de um salário, sabendo que a taxa de ICMS a ser aplicado ao salario é TAXA_ICMS1 se o salário é menor que 10000, ou TAXA_ICMS2, caso contrário
+ */
+
+#include <stdio.h>
+
+#define TAX_ICMS1 5
+#define TAX_ICMS2 17
+#define VAL_ICMS(salary) (((salary) < 10000) ? (TAX_ICMS1): (TAX_ICMS2))
+
+float net_salary(float, int);
+
+int main(void) {
+   float salary;
+   printf("Informe o valor do salário: ");
+   scanf("%f", &salary);
+   printf("Taxa ICMS = %d\nSalário descontado = %.2f\n", VAL_ICMS(salary), net_salary(salary, VAL_ICMS(salary)));
+
+   return 0;
+}
+
+float net_salary(float salary, int tax) {
+   return salary - (salary * tax / 100);
+}
